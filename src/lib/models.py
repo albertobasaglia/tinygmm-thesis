aa2aace08007c5e933401c52fc96b016445ce67f
Alberto Basaglia <alberto.basaglia17@gmail.com>
2026-03-30 08:24:34 +0200

    add dropout to training

--- src/lib/models.py
+++ src/lib/models.py
@@ -16,9 +16,13 @@ class SpeechFeatureExtractor(nn.Module):
 
         self.backbone = nn.Sequential(
             nn.Conv2d(1, 32,  3, padding=1), nn.BatchNorm2d(32),  nn.ReLU(), nn.MaxPool2d(2),
+            nn.Dropout2d(0.5),
             nn.Conv2d(32, 64, 3, padding=1), nn.BatchNorm2d(64),  nn.ReLU(), nn.MaxPool2d(2),
+            nn.Dropout2d(0.5),
             nn.Conv2d(64, 128,3, padding=1), nn.BatchNorm2d(128), nn.ReLU(),
+            nn.Dropout2d(0.5),
             nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Dropout(0.5),
             nn.Flatten(),
         )
         self.embedding_head = nn.Sequential(
